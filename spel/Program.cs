dabb8c3601e3393942c95b5ca5c7ce0e403e4ea5
Karin Skans <skka19st@student.ju.se>
2019-11-25 18:16:00 +0100

    version01

---
+++ spel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace spel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // skapa objekt av klassen cSpel 
+            cSpel spelare = new cSpel();
+
+            // 
+            Console.WriteLine("Vänta på texten 'Tryck' innan du trycker");
+
+            // slumpvis fördröjning för att testa reaktionen
+            spelare.SlumpvisDelay();
+
+            // starta spelet
+            spelare.StartaSpelet();
+
+            // kontroll att spelaren inte tjuvstartat
+            // läs in tecken från indata-strömmen
+            if (Console.KeyAvailable == true) {
+                Console.WriteLine("Hallå där, du försöker fuska!! Börja om.");
+                ConsoleKeyInfo rensaPipen = Console.ReadKey();
+                return;
+            } 
+
+            // här går startsignalen
+            Console.WriteLine("TRYCK!!");
+            ConsoleKeyInfo svar = Console.ReadKey();
+
+            // stoppa spelet
+            spelare.StoppaSpelet();
+
+            // beräkna reaktionstiden
+            spelare.BeraknaReaktionstid();
+
+            // presentera resultat
+            Console.WriteLine("");
+            Console.WriteLine("reaktionstid i millisekunder: " + spelare.Reaktion);
+        }
+    }
+}
---
+++ spel/cSpel.cs
@@ -0,0 +1,48 @@
+// namespace som används
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace spel
+{
+    // public = klassen kan användas av andra projekt
+    // private = nås endast av den egna klassen
+    // standard är 'internal' = endast det egna projektet kan använda
+    public class cSpel {
+        // klassens attribut
+        private int start;
+        private int stopp;
+        private int reaktion;
+
+        // hämtar TickCount för start
+        public void StartaSpelet() {
+            start = Environment.TickCount;
+        }
+    
+        // hämtar TickCount för spelarens stopp-signal
+        public void StoppaSpelet() {
+            stopp = Environment.TickCount;
+        }
+    
+        // slumpvis fördröjning 3-10 sek 
+        public void SlumpvisDelay() {
+            Random slumpen = new Random();
+            int antSek = slumpen.Next(3,11);
+            antSek *= 1000;
+            Thread.Sleep(antSek);
+        }
+
+        // beräknar spelarens reaktions-tid i millisekunder
+        public void BeraknaReaktionstid() {
+            reaktion = stopp - start;            
+        }
+            
+        // property/egenskap för reaktionstiden 
+        public int Reaktion { 
+            // get returnerar värdet på attributet
+            get {
+                return reaktion;
+            } 
+        }
+    }
+}
