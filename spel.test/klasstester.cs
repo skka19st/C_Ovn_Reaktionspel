8f11b00e2121b036f261cfd5c8b08184b4b0e154
Karin Skans <skka19st@student.ju.se>
2019-11-28 13:41:06 +0100

    tillagg i test

--- spel.test/klasstester.cs
+++ spel.test/klasstester.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.Threading;
 
 namespace spel.test
 {
@@ -16,6 +17,25 @@ namespace spel.test
             // assert - säkerställa resultat
             // förväntat värde, faktiskt värde
             Assert.NotNull(testobj1);
+        }
+        [Fact]
+        public void TestaBerakning() 
+        {
+            // arrange - testdata 
+            var testobj1 = new cSpel();
+            var forvantatResultat = 1000;
+
+
+            // act - test-case
+            testobj1.StartaSpelet();
+            Thread.Sleep(forvantatResultat);
+            testobj1.StoppaSpelet();
+            testobj1.BeraknaReaktionstid();
+
+            // assert - säkerställa resultat
+            // förväntat värde, faktiskt värde
+            Assert.Equal(forvantatResultat, testobj1.Reaktion);
+
         }
     }
 }
